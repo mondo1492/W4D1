4f6b9f1b0172c377acdb65b6abf24a721bd86a02
Aaron Mondshine <aaron.mondshine@gmail.com>
2017-06-19 15:32:06 -0700

    finish first_routes_yay, create update and destroy methods

--- first_routes_yay/app/controllers/users_controller.rb
+++ first_routes_yay/app/controllers/users_controller.rb
@@ -1,16 +1,61 @@
 class UsersController < ApplicationController
   def index
-    # @users = User.all?
-    # render JSON: @users
-    render text: "We're good at this!"
+    @users = User.all
+    render json: @users
   end
 
   def create
-    render json: params
-
+    user = User.new(user_params)
+    if user.save
+      render json: user
+    else
+      render(
+      json: user.errors.full_messages, status: :unprocessable_entity
+    )
+    end
   end
 
   def show
-    render json: params
+    @user = User.find_by(id: params[:id])
+    if @user
+      render json: @user
+    else
+      render(
+      json: ['Cannot find User'], status: 404
+    )
+    end
   end
+
+  def update
+    @user = User.find(params[:id])
+    if @user
+      @user.update_attributes!(user_params)
+      render json: @user
+    else
+      render(
+      json: @user.errors.full_messages, status: 400
+    )
+    end
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    if @user
+      @user.destroy
+      render json: @user
+    else
+      render(
+      json: ['We cant delete what does not exist'], status: 404
+    )
+    end
+  end
+
+
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+
+
 end
